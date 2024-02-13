5f320f4fd06933c5efd920d90597fb80291dad5f
Emmanoel Dantas <emmanuel2011.07@gmail.com>
2024-02-13 20:07:30 -0300

    refactor: correcting routes and imports

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,7 @@
 
-import { CompaniesController } from "../controllers/CompaniesController";
+import { CompaniesController } from "../controllers/company.controller";
 import { Router } from "express";
-import { JobController } from "../controllers/JobController";
+import { JobController } from "../controllers/job.controller";
 
 const routes = Router();
 
@@ -11,6 +11,15 @@ routes.get('/companies/:id', new CompaniesController().getCompany);
 
 routes.post('/job', new JobController().create);
 
-routes.put('/job/:job_id/pusblish', new JobController().create);
+// endpoint teste findAll jobs
+routes.get('/job', new JobController().index);
+
+routes.put('/job/:id/publish', new JobController().updateStatus);
+
+routes.put('/job/:id', new JobController().editPost);
+
+routes.delete('/job/:job_id', new JobController().deletePostDraft)
+
+routes.put('/job/:id/archived', new JobController().setStatusArchive);
 
 export { routes }
